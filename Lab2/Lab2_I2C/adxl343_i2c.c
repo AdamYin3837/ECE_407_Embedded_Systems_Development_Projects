0ffecc2452091ed6de4fba7ce327e39572300938
Uzair Tariq <uzair@wrongsyntax.dev>
2026-02-19 12:14:39 -0700

    re-organize lab2 files, add screenshots

--- Lab2/Lab2_I2C/adxl343_i2c.c
+++ Lab2/Lab2_I2C/adxl343_i2c.c
@@ -10,26 +10,6 @@
 #include "pico/binary_info.h"
 #include "hardware/i2c.h"
 
-/* Example code to talk to a ADXL343 MEMS accelerometer and gyroscope
-
-   This is taking to simple approach of simply reading registers. It's perfectly
-   possible to link up an interrupt line and set things up to read from the
-   inbuilt FIFO to make it more useful.
-
-   NOTE: Ensure the device is capable of being driven at 3.3v NOT 5v. The Pico
-   GPIO (and therefore I2C) cannot be used at 5v.
-
-   You will need to use a level shifter on the I2C lines if you want to run the
-   board at 5v.
-
-   Connections on Raspberry Pi Pico board, other boards may vary.
-
-   GPIO PICO_DEFAULT_I2C_SDA_PIN (On Pico this is GP4 (pin 6)) -> SDA on ADXL343 board
-   GPIO PICO_DEFAULT_I2C_SCL_PIN (On Pico this is GP5 (pin 7)) -> SCL on ADXL343 board
-   3.3v (pin 36) -> VCC on ADXL343 board
-   GND (pin 38)  -> GND on ADXL343 board
-*/
-
 // ADXL343 alternate I2C address
 static int addr = 0x53;
 
